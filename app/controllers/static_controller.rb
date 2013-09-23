6a8a7f75f57688aeb76a9da5b36124460bfb25d8
David Frey <perfectritone@gmail.com>
2013-09-22 23:44:07 -0400

    material partial created and sent each material

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,23 @@
 class StaticController < ApplicationController
   def index
     @title = "BBB"
+
+    material_subclasses = Material.descendants
+    @material_subclass_featured = {}
+    material_subclasses.each do |sc|
+      @material_subclass_featured[sc.name] = sc.where(featured: true).limit(4).shuffle
+    end
   end
 end
+
+=begin
+  
+  1: get a list of subclasses
+  2: each of those subclasses needs to bring with itself its 
+    Class name (for link)
+    4 featured examples
+  3: send these to a partial
+  4: all the partials are rendered on the index page
+  
+=end
+
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,10 +4,12 @@ Bbb::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Do not eager load code on boot.
-  config.eager_load = false
+  #
+  # Overwritten so that children of Material are loaded.
+  config.eager_load_paths = Dir[Rails.root.join('app', 'models')]
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
